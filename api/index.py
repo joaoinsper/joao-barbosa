87a32f1e30cd852bca1f733a2805859c33b1bc51
joaobarb0sa <jv.sbarbosa@hotmail.com>
2024-03-11 22:11:54 -0300

    estudantes

--- api/index.py
+++ api/index.py
@@ -9,3 +9,14 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/teste/<id>')
+def estudante(id):
+    return turma[id]
+
+    
+turma = {"1": {"nome": "João", "idade": 20, "curso": "Engenharia"},
+         "2": {"nome": "Maria", "idade": 22, "curso": "Medicina"},
+         "3": {"nome": "José", "idade": 21, "curso": "Direito"},
+         "4": {"nome": "Ana", "idade": 23, "curso": "Administração"},
+         "5": {"nome": "Pedro", "idade": 24, "curso": "Contabilidade"}}
